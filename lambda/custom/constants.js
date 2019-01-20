d3350c5ba4a1c5643d6cbbd5e05af5f86a74fe24
andrelandgraf <andre.timo.landgraf@gmail.com>
2019-01-20 15:39:11 +0100

    added further unit tests with bespoken

--- lambda/custom/constants.js
+++ lambda/custom/constants.js
@@ -20,13 +20,13 @@ const outputSpeech = {
     'error': 'Sorry, I can\'t understand the command. Please say again.',
     'help': 'I am your virtual Mercedes Assistant. I can lock your car and more!',
     'lock': 'Your Mercedes is now locked.',
-    'locked': 'Your Mercedes is locked',
-    'notlocked': 'Your Mercedes is not locked',
+    'locked': 'Your Mercedes is locked.',
+    'notlocked': 'Your Mercedes is not locked.',
     'couldnotLock': 'There was a problem, try to lock your car later.',
     'authRequired': 'You must authenticate with your Mercedes Account to use this skill.',
-    'open': 'There is at least one door open',
-    'closed': 'All doors are closed',
-    'whichDoor': 'is open',
+    'open': 'There is at least one door open.',
+    'closed': 'All doors are closed.',
+    'whichDoor': 'is open.',
 }
 
 module.exports = {
